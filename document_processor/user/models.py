4b0c467c65b042e9195d05ebec565ca7734d6000
Mustahid Hasan <mustahidhasan9@gmail.com>
2025-02-02 21:33:35 +0600

    document time is added

--- document_processor/user/models.py
+++ document_processor/user/models.py
@@ -12,6 +12,8 @@ class UploadedFile(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE
     )  # Associate each file with a specific user
+    file_created_at = models.DateTimeField(auto_now=True)
+    is_archieved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.file.name
