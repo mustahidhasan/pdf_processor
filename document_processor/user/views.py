54f9f433c3b1d41ecf97739e829092fec07eaae9
Mustahid Hasan <mustahidhasan9@gmail.com>
2025-02-04 02:32:53 +0600

    register removed

--- document_processor/user/views.py
+++ document_processor/user/views.py
@@ -95,18 +95,19 @@ def logout_user(request):
 
 
 def register_user(request):
-    if request.method == "POST":
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # authenticate and login
-            username = form.cleaned_data["username"]
-            password = form.cleaned_data["password1"]
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            messages.success(request, "You have successfully Registered")
-            return redirect("home")
-    else:
-        form = SignUpForm()
-        return render(request, "register.html", {"form": form})
-    return render(request, "register.html", {"form": form})
+    return redirect("home")
+    # if request.method == "POST":
+    #     form = SignUpForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         # authenticate and login
+    #         username = form.cleaned_data["username"]
+    #         password = form.cleaned_data["password1"]
+    #         # user = authenticate(username=username, password=password)
+    #         # login(request, user)
+    #         messages.success(request, "You have successfully Registered")
+    #         return redirect("home")
+    # else:
+    #     # form = SignUpForm()
+    #     # return render(request, "register.html", {"form": form})
+    # return render(request, "register.html", {"form": form})
